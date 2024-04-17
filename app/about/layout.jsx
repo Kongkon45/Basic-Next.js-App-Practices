38f6246ad295d80d2d65d27a2951a24e41194c87
Kongkon Jowarder <kongkon4545@gmail.com>
2024-04-17 15:26:09 +0600

    image and font optimization

--- app/about/layout.jsx
+++ app/about/layout.jsx
@@ -1,9 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
+import { Roboto } from 'next/font/google'
+ 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata = {
+  title: "About page",
+  description: "This is about page description",
+};
 
 const AboutLayout = ({children}) => {
   return (
-    <div className='mt-6'>
+    <div className={roboto.className}>
         <nav>
             <ul className="flex justify-center items-center gap-10 h-10 bg-blue-500 text-white text-xl font-bold">
                 <Link href="/about/mission"><li>Mission</li></Link>
--- app/about/mission/page.jsx
+++ app/about/mission/page.jsx
@@ -1,12 +1,15 @@
 
 import { Button } from '@/app/components/Button'
+import Image from 'next/image'
 import React from 'react'
+import myImg from "@/public/images/Kongkon.jpg"
 
 const Mission = () => {
   return (
     <main>
         <div className='mt-6'>Mission page</div>
         <Button/>
+        <Image src={myImg} alt="my image" quality={100} height={200} width={200}/>
     </main>
   )
 }
